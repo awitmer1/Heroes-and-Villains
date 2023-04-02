22f51a6124398c2b4f7abb7706c1d55f362181e4
Andrew Witmer <witmer.a@gmail.com>
2023-04-01 17:47:34 -0700

    Adjusted URL paths in 'urls.py'; Began Writing View Functions for Heroes Table API

---
+++ supers/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+## Reflect ending of URL paths + link to API Views ##
+## App URLs = Endpoint URLs ##
+urlpatterns = [
+    path('', views.supers_list),
+    path('<int:pk>/', views.supers_list),
+]
--- supers/views.py
+++ supers/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from .serializer import SupersSerializer
+from .models import Supers
 
 # Create your views here.
+@api_view(['GET', 'POST'])
+
+def supers_list(request):
+
+    if request.method == 'GET':
+        
+        supers = Supers.objects.all()
+        serialzer = SupersSerializer(supers, many=True)
+        return Response(serialzer.data)
