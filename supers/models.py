bf7cab08f99723d9b5ceb162be5f72d2f884aedd
Andrew Witmer <witmer.a@gmail.com>
2023-04-01 17:36:39 -0700

    Creaded ForeignKey Relationship b/w Supers and SuperTypes Models; Adjusted Supers Serializer for Addl Field

--- supers/models.py
+++ supers/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from super_types.models import SuperTypes
 
 # Create your models here.
 class Supers(models.Model):
@@ -7,4 +8,4 @@ class Supers(models.Model):
     primary_ability = models.CharField(max_length=255)
     secondary_ability = models.CharField(max_length=255)
     catchphrase = models.CharField(max_length=255)
-    # super_type = models.ForeignKey 
+    super_type = models.ForeignKey(SuperTypes, on_delete=models.CASCADE)
